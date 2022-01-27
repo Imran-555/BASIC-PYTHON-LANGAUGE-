14292f5042dc8d323a0404dc788ebc0234b9dad9
IMRAN RAEENI <74055781+Imran-555@users.noreply.github.com>
2022-01-27 12:25:53 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,49 @@
+                                      #### EASY TO LEARN PYTHON  #####
+                      # FOR BETTER UNDERSTANDING READ CAREFULLY EVERY COMMENT #
+
+
+
+# 1) for print any string use print(" ") for example
+print("MY NAME IS JHON ")
+
+
+# for print any value we do'nt use ""  for example
+a=5          # 5 is store in a variable "a"
+print(a)     # print the value of "a"
+
+
+
+# 2) use of operator  (+ - * / )
+num1= 12             #12 is store in variable name "num1"
+num2=15              #12 is store in variable name "num2"
+print(num1+num2)     #use of +
+print(num1-num2)     #use of -
+print(num1*num2)     #use of *
+print(num1/num2)     #use of /
+
+
+
+
+                       ## 3) use of Input function , we use input to take input by user ##
+
+#take string as input
+name=input("Please Enter Your Name ")  #this will store the input in the variable "name"
+print("Welcome " + name)
+
+
+#take integer value as input
+#use int before the function input as example
+value=int(input("Enter the value "))   #It will take input as integer and store it in the variable name "value"
+
+
+
+
+#program using input function
+# addition of two number
+first=int(input("Enter the first number : "))
+second=int(input("Enter the second number : "))
+print("The sum of first and second number : ")
+print(first+second)
+
+
+
